78f645e3918e8c2f4d726515fbc1f1f4897b1467
Danil Rybnikov <danil.rybnikov@mail.ru>
2023-12-13 22:45:47 +0700

    Second commit

--- src/Predator.java
+++ src/Predator.java
@@ -1,5 +1,9 @@
 public abstract class Predator extends Beast{
+    public Predator(int maxSize, int speed, int maxSatiety) {
+        super(maxSize, speed, maxSatiety);
+    }
+
     public void eat(Beast beast){
-        if (beast.alive)
+        if (beast.alive);
     }
 }
